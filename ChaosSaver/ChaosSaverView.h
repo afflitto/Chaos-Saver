413b0253a0e0a305dadabd5856a6e5248dafe188
Andrew Afflitto <andrew.afflitto@gmail.com>
2019-03-03 23:45:38 -0500

    Initial Commit

---
+++ ChaosSaver/ChaosSaverView.h
@@ -0,0 +1,13 @@
+//
+//  ChaosSaverView.h
+//  ChaosSaver
+//
+//  Created by Andrew Afflitto on 3/3/19.
+//  Copyright © 2019 Andrew Afflitto. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ChaosSaverView : ScreenSaverView
+
+@end
